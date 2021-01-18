ddad2bfdca600b8ab7658a561dd43b2a64fbaa56
Emily Eubanks <coffeefoliaforensics@gmail.com>
2021-01-18 07:59:52 +0000

    initial commit

--- main.go
+++ main.go
@@ -1,7 +1,10 @@
 package main
 
 import (
-	"os"
+	"io/ioutil"
+	"math/rand"
+	"strings"
+	"time"
 )
 
 func check(e error) {
@@ -11,8 +14,22 @@ func check(e error) {
 }
 
 func main() {
-	fname := os.Args[1]
-	file, err := os.Open(fname)
+	// seed random with time
+	rand.Seed(time.Now().UTC().UnixNano())
+	// read in dictionary file
+	f, err := ioutil.ReadFile("dict.txt")
 	check(err)
+	l := strings.Split(string(f), "\n")
 
+	for i := 0; i < 10; i++ { // combine and print word mash
+		lenl := len(l) - 1
+		a := rand.Intn(lenl - 1)
+		b := rand.Intn(lenl - 1)
+		c := l[a] + l[b]
+		if len(c) > 15 {
+			println(c[:15])
+		} else {
+			println(c)
+		}
+	}
 }
